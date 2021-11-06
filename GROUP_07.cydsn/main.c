711b23193bbc5c685703511a905623d60fd9ffb1
veronicafossati <veronica.fossati@mail.polimi.it>
2021-11-06 11:30:23 +0100

    inizializzazioni, to do I2C

--- GROUP_07.cydsn/main.c
+++ GROUP_07.cydsn/main.c
@@ -10,12 +10,28 @@
  * ========================================
 */
 #include "project.h"
+#include "InterruptRoutines.h"
+#define LED_ON 1
+#define LED_OFF 0
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    UART_Start();
+    
+    EZI2C_Start();
+    
+    ADC_DelSig_Start();
+   
+    Mux_Start();
+    
+    isr_ADC_Start();
+    
+    Timer_Start();
+    
+   
 
     for(;;)
     {
